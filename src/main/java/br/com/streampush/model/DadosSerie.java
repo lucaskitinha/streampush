c67abc4fcd45ba0ca3fb640b400fe945e87a2482
Lucas Mesquita <lucas.mesquita@perinity.com>
2024-05-30 21:12:51 -0300

    Commit inicial

---
+++ src/main/java/br/com/streampush/model/DadosSerie.java
@@ -0,0 +1,13 @@
+package br.com.streampush.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DadosSerie(
+
+		@JsonAlias("Title") String titulo,
+		@JsonAlias("totalSeasons") Integer totalTemporada,
+		@JsonAlias("imdbRating") String avaliacao
+) {
+}
---
+++ src/test/java/br/com/streampush/StreampushApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.streampush;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreampushApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
